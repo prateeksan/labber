65a9ba615c00a43a764ae3e4a5ae1cb32771a32a
prateeksan <sanyal.prateek@gmail.com>
2016-02-21 22:02:00 -0500

    home controller added, posts ordered in desc by updated_at

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :posts
   root "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
