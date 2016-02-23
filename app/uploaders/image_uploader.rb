0835cd5f55c8c1901fee94b862e8fa967fbd5e1f
prateeksan <sanyal.prateek@gmail.com>
2016-02-22 19:48:40 -0500

    functionality for adding images and avatars done, need to add edit functionality for avatars

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,6 +35,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumb do
     process resize_to_fill: [100, 100]
   end
+  
+  version :resized do
+    # returns an image with a maximum width of 100px 
+    # while maintaining the aspect ratio
+    # 10000 is used to tell CW that the height is free 
+    # and so that it will hit the 100 px width first
+    process :resize_to_fit => [400, 10000]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
