f41ba4fa04a15867280e74817a36a3a6c18645aa
prateeksan <sanyal.prateek@gmail.com>
2016-02-22 20:49:41 -0500

    created patch method in custom user controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :posts
   root "home#index"
+
+  resource :user, only: [:edit] do
+    collection do
+      patch 'update_email'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
